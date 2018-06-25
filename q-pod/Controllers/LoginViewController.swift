4bb4b2468f005c5e8813cbbca52e962ad6ae94cd
Murrali <murrali@kingpick.com>
2018-06-25 23:16:49 +0800

    User Registration and Logging In

---
+++ q-pod/Controllers/LoginViewController.swift
@@ -0,0 +1,95 @@
+//
+//  LoginViewController.swift
+//  q-pod
+//
+//  Created by Murrali Ramasamy on 25/6/18.
+//  Copyright © 2018 Murrali Ramasamy. All rights reserved.
+//
+
+import UIKit
+import FirebaseAuth
+import FirebaseUI
+import FirebaseDatabase
+
+typealias FIRUser = FirebaseAuth.User
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var outLogin: UIButton!
+    
+    @IBAction func loginPressed(_ sender: UIButton) {
+        print("Login button pressed.")
+        // 1
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else { return }
+        
+        // 2
+        authUI.delegate = self
+        
+        // 3
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
+    }
+    
+    /***************************************************
+     1. access the FUIAuth default auth UI singleton
+     2. set FUIAuth's singleton delegate
+     3. present the auth view controller
+     ***************************************************/
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension LoginViewController: FUIAuthDelegate {
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?) {
+        
+        if let error = error {
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        
+        // 1
+        guard let user = user
+            else { return }
+        
+        // 2
+        let userRef = Database.database().reference().child("users").child(user.uid)
+        
+        UserService.show(forUID: user.uid) { (user) in
+            if let user = user {
+                // handle existing user
+                User.setCurrent(user)
+                
+                let initialViewController = UIStoryboard.initialViewController(for: .main)
+                self.view.window?.rootViewController = initialViewController
+                self.view.window?.makeKeyAndVisible()
+                
+            } else {
+                // handle new user
+                self.performSegue(withIdentifier: Constants.Segue.toCreateUsername, sender: self)
+            }
+        }
+        
+    }
+}
---
+++ q-pod/Supporting/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  q-pod
+//
+//  Created by Murrali Ramasamy on 25/6/18.
+//  Copyright © 2018 Murrali Ramasamy. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    struct Segue {
+        static let toCreateUsername = "toCreateUsername"
+    }
+}
